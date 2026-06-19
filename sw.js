32b0afe94d60ec773e553ef58d88235fa73beb80
matteoferrari14 <matteo.ferrari14@gmail.com>
2026-06-19 16:11:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        return response || fetch(event.request);
+        return response || fill(event.request);
       })
   );
 });
